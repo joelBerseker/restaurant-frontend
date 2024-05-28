f498406eac2dc8c6f89269a1f4e47522d2c0caeb
leowi <jhoshep_1999@hotmail.com>
2024-05-28 14:35:21 -0500

    some changes

---
+++ src/helpers/constants/globalSettings.js
@@ -0,0 +1,9 @@
+const globalSettings = {
+  text_min_size: 3,
+  normal_text_max_size: 100,
+  large_text_max_size: 1000,
+  number_max_size: 9,
+  number_decimal_max_size: 10,
+};
+
+export { globalSettings };

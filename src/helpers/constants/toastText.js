8cfa95edd0d9e04df6571e836e0fa9f95f0554e5
leowi <jhoshep_1999@hotmail.com>
2024-05-28 14:02:37 -0500

    toast tex

---
+++ src/helpers/constants/toastText.js
@@ -0,0 +1,40 @@
+const toastText = {
+  add_success: {
+    text: "El registro se agrego correctamente.",
+    type: 1,
+  },
+  add_error: {
+    text: "No se pudo agregar el registro, si continúa sucediendo contacte con su proveedor.",
+    type: 2,
+  },
+  edit_success: {
+    text: "El registro de edito correctamente.",
+    type: 1,
+  },
+  edit_error: {
+    text: "No se pudo editar el registro, si continúa sucediendo contacte con su proveedor.",
+    type: 2,
+  },
+  delete_success: {
+    text: "El registro se elimino correctamente.",
+    type: 1,
+  },
+  delete_error: {
+    text: "No se pudo eliminar el registro, si continúa sucediendo contacte con su proveedor.",
+    type: 2,
+  },
+  get_element_error: {
+    text: "No se pudo obtener el registro, si continúa sucediendo contacte con su proveedor.",
+    type: 2,
+  },
+  get_list_error: {
+    text: "No se pudo obtener los registros, si continúa sucediendo contacte con su proveedor.",
+    type: 2,
+  },
+  validation_error: {
+    text: "Datos no válidos, revise si todos los campos se llenaron correctamente.",
+    type: 2,
+  },
+};
+
+export { toastText };

e7c3a19aae14eb85a5b9ea6d5af532cddc1cfdef
joelBersek <jatamaria@unsa.edu.pe>
2024-05-29 17:53:38 -0500

    edit dataTransform 2.0

--- src/services/transform/dataTransform.js
+++ src/services/transform/dataTransform.js
@@ -2,8 +2,21 @@ function transformApiData(apiData, modelInstance) {
   const data = new modelInstance();
 
   for (const key in apiData) {
-    if (data.hasOwnProperty(key) && apiData.hasOwnProperty(key)) {
-      data[key].value = apiData[key];
+    if (apiData.hasOwnProperty(key)) {
+      // Check if the key has a corresponding property in the data model
+      if (data.hasOwnProperty(key)) {
+        data[key].value = apiData[key];
+      }
+
+      // Check for additional data keys starting with "__"
+      const additionalKey = `_${key}`;
+      if (apiData.hasOwnProperty(additionalKey)) {
+        if (!data[key]) {
+          data[key] = {};
+        }
+        data[key].value = apiData[key];
+        data[key].additional = apiData[additionalKey];
+      }
     }
   }
 

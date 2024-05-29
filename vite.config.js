a1da095fb97513e93116679b926c33f42e2733d0
joelBersek <jatamaria@unsa.edu.pe>
2024-05-29 12:00:45 -0500

    add .env

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/" // Establece el valor de BASE_URL en producción
+      : "/",
+  plugins: [vue(), VueDevTools()],
+  build: {
+    sourcemap: true, // Habilita la generación de source maps en producción
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});

871e6043a2f490e5891d78571921ca86f65fd044
joelBersek <jatamaria@unsa.edu.pe>
2024-06-03 13:50:48 -0500

    add role

---
+++ src/models/user/RolModel.js
@@ -0,0 +1,32 @@
+import { Model } from "@/models/";
+
+export class RolModel extends Model {
+  name = {
+    id: "name",
+    name: "Nombre",
+
+    value: null,
+
+    validation: {},
+    validate: ["length"],
+  };
+  description = {
+    id: "description",
+    name: "Descripción",
+    type: "textarea",
+
+    value: null,
+    default: "Sin descripción",
+
+    required: false,
+    validation: {},
+    validate: ["length"],
+  };
+
+  getDataOptions() {
+    return {
+      value: this.id.value,
+      text: this.name.value,
+    };
+  }
+}

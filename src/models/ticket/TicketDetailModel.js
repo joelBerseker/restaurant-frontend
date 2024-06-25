e1c45fdeb1da3927ffb01b80e274b71ab77cd14a
joelBersek <jatamaria@unsa.edu.pe>
2024-06-25 13:30:34 -0500

    edit model TicketDetailModel

--- src/models/ticket/TicketDetailModel.js
+++ src/models/ticket/TicketDetailModel.js
@@ -5,15 +5,19 @@ export class TicketDetailModel extends Model {
     id: "ticket_id",
     name: "Ticket",
     value: null,
+    aditional: {},
+    validation: {},
+    validate: ["select"],
   };
 
-  quantity = {
-    id: "quantity",
-    name: "Cantidad",
-    type: "number",
+  is_menu = {
+    id: "is_menu",
+    name: "Es Menu",
     value: null,
+    required: false, // null=True, blank=True in Django
     validation: {},
-    validate: ["length", "number"],
+    validate: ["boolean"],
+    default: true,
   };
 
   product_id = {
@@ -22,18 +26,47 @@ export class TicketDetailModel extends Model {
     value: null,
     aditional: {},
     validation: {},
+    validate: ["number"],
+  };
+
+  price = {
+    id: "price",
+    name: "Precio Total",
+    value: null,
+    validation: {},
+    validate: ["decimal"],
+    default: 0,
+  };
+
+  quantity = {
+    id: "quantity",
+    name: "Cantidad",
+    value: null,
+    required: false, // null=True, blank=True in Django
+    validation: {},
+    validate: ["number"],
   };
 
   description = {
     id: "description",
     name: "Descripción",
+    type: "textarea",
     value: null,
+    default: "Sin descripción",
     required: false, // null=True, blank=True in Django
-
     validation: {},
     validate: ["length"],
-    default: "Sin descripción",
   };
+
+  status = {
+    id: "status",
+    name: "Estado",
+    value: null,
+    validation: {},
+    validate: ["select"],
+    default: 1,
+  };
+
   initModel(data) {
     this.ticket_id.value = data;
   }
